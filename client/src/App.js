28eae57f48225d065db06d5fbd9752c89f706fef
charles <charles.fotie@epitech.eu>
2024-07-26 17:01:04 +0200

    feat: register/login

--- client/src/App.js
+++ client/src/App.js
@@ -1,25 +1,21 @@
-import logo from './logo.svg';
+
+import React from 'react';
 import './App.css';
+import Register from './Register';
+import Login from './Login';
+
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+    return (
+        <div className="App">
+            <header className="App-header">
+                <h1>Welcome to React and Symfony Integration</h1>
+                <Register />
+                <Login />
+            </header>
+        </div>
+    );
 }
 
 export default App;
+
