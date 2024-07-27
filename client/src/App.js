c57bbe7e84159dcf2c5dbab8b41403863b743d6a
charles <charles.fotie@epitech.eu>
2024-07-27 17:29:13 +0200

    fix: get admin on localhost 8000

--- client/src/App.js
+++ client/src/App.js
@@ -3,6 +3,7 @@ import React from 'react';
 import './App.css';
 import Register from './Register';
 import Login from './Login';
+import Admin from './Admin';
 
 
 function App() {
@@ -12,6 +13,7 @@ function App() {
                 <h1>Welcome to React and Symfony Integration</h1>
                 <Register />
                 <Login />
+                <Admin />
             </header>
         </div>
     );
