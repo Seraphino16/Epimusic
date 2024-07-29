3b44cd413f65603334f8a5d58f39dc0b63ff1af3
sopcharles <151517657+sopcharles@users.noreply.github.com>
2024-07-29 10:14:01 +0200

    fix: administrator constraint (#6)

--- client/src/App.js
+++ client/src/App.js
@@ -6,20 +6,42 @@ import ProductAdminEdit from './components/forms/ProductAdminEdit';
 import Register from './components/AuthForms/Register';
 import Login from './components/AuthForms/Login';
 import Navbar from './components/navbar/Navbar';
+import ProtectedRoute from './components/ProtectedRoute';
 
 const App = () => {
-    return (
-        <Router>
-            <Navbar />
-            <Routes>
-                <Route path="/register" element={<Register />} />
-                <Route path="/login" element={<Login />} />
-                <Route path="/admin" element={<ProductAdminList />} />
-                <Route path="/admin/create-product" element={<ProductAdminForm />} />
-                <Route path="/admin/edit-product/:id" element={<ProductAdminEdit />} />
-            </Routes>
-        </Router>
-    );
+  return (
+    <Router>
+      <Navbar />
+      <Routes>
+        <Route path="/register" element={<Register />} />
+        <Route path="/login" element={<Login />} />
+        <Route
+          path="/admin"
+          element={
+            <ProtectedRoute requiredRole="admin">
+              <ProductAdminList />
+            </ProtectedRoute>
+          }
+        />
+        <Route
+          path="/admin/create-product"
+          element={
+            <ProtectedRoute requiredRole="admin">
+              <ProductAdminForm />
+            </ProtectedRoute>
+          }
+        />
+        <Route
+          path="/admin/edit-product/:id"
+          element={
+            <ProtectedRoute requiredRole="admin">
+              <ProductAdminEdit />
+            </ProtectedRoute>
+          }
+        />
+      </Routes>
+    </Router>
+  );
 };
 
 export default App;
