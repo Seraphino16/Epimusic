7198073c96a02ff532880fecc9256d068119f85b
charles <charles.fotie@epitech.eu>
2024-07-30 17:29:24 +0200

    fix: colorFilteredArticles

--- client/src/components/Filtered/FilterDisclosure.js
+++ client/src/components/Filtered/FilterDisclosure.js
@@ -6,7 +6,7 @@ import FilterOptions from './FilterOptions';
 const FilterDisclosure = ({ section }) => (
   <Disclosure as="div" className="border-t border-gray-200 px-4 py-6 lg:py-0 lg:border-0">
     <h3 className="border mx-2 my-3 flow-root rounded-lg">
-      <DisclosureButton className="group flex w-full items-center justify-between border bg-white px-2 py-3 text-gray-400 hover:text-gray-500">
+      <DisclosureButton className="group flex w-full items-center justify-between border bg-white px-2 py-3 text-gray-400 hover:bg-gray-100">
         <span className="font-medium text-gray-900">{section.name}</span>
         <span className="ml-6 flex items-center">
           <PlusIcon aria-hidden="true" className="h-5 w-5 group-data-[open]:hidden" />
