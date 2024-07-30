338ffaac2994b92c57eeeffa25a4c2f69a674a4c
charles <charles.fotie@epitech.eu>
2024-07-30 17:07:31 +0200

    fix: filterdArticles

--- client/src/components/Filtered/FilterOptions.js
+++ client/src/components/Filtered/FilterOptions.js
@@ -12,7 +12,7 @@ const FilterOptions = ({ options, sectionId }) => (
           type="checkbox"
           className="h-4 w-4 rounded border-gray-300 text-indigo-600 focus:ring-indigo-500"
         />
-        <label htmlFor={`filter-${sectionId}-${optionIdx}`} className="ml-3 min-w-0 flex-1 text-gray-500">
+        <label htmlFor={`filter-${sectionId}-${optionIdx}`} className="ml-3 min-w-0 flex-2 text-gray-500 text-sm lg:text-base">
           {option.label}
         </label>
       </div>
