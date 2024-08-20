ecbbbc0921495cbf4ce6b34bbe8cf71a2e1c7b09
Séraphin <seraphinbnt@gmail.com>
2024-08-20 17:29:18 +0200

    feat: api-> get musicoins from a user id

--- server/src/Controller/MusicoinController.php
+++ server/src/Controller/MusicoinController.php
@@ -6,13 +6,14 @@ use App\Entity\Musicoin;
 use Doctrine\ORM\EntityManagerInterface;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\JsonResponse;
+use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Attribute\Route;
 
 #[Route('/api/musicoin')]
 class MusicoinController extends AbstractController
 {
-    #[Route('/', name:'get_musicoin_info')]
+    #[Route('/', name:'get_musicoin_info', methods:["GET"])]
     public function getMusicoin(EntityManagerInterface $entityManager): JsonResponse
     {
         $musicoins = $entityManager->getRepository(Musicoin::class)->findAll();
@@ -37,6 +38,34 @@ class MusicoinController extends AbstractController
                 "isPlayedToday" => $isPlayedToday,
             ];
         }
+
+        return new JsonResponse($data);
+    }
+
+    #[Route('/user/{id}', name: 'get_musicoin_from_user', methods:["GET"])]
+    public function getMusicoinFromUserId(EntityManagerInterface $entityManager, int $id): JsonResponse
+    {
+        $musicoin = $entityManager->getRepository(Musicoin::class)->findOneBy(["user" => $id]);
+
+        if (!$musicoin) {
+            return new JsonResponse(["message" => "Les musicoins n'ont pas pu être trouvés"]);
+        }
+
+        $today = new \DateTime();
+        
+        $lastGameDate = $musicoin->getLastGameDate();
+
+        if ($lastGameDate) {
+            $isPlayedToday = ($lastGameDate->format("Y-m-d") === $today->format("Y-m-d"));
+        } else {
+            $isPlayedToday = false;
+        }
+
+        $data = [
+            "userId" => $musicoin->getUser()->getId(),
+            "quantity" => $musicoin->getQuantity(),
+            "isPlayedToday" => $isPlayedToday,
+        ];
 
         return new JsonResponse($data);
     }
