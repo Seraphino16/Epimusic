0238fcf7ae6cbb9f2b2fb90a19ce9814192c74d7
emilie <emilie.le-lan@epitech.eu>
2024-08-26 16:08:07 +0200

    feat: add CRUD for providers on admin pannel

--- server/src/Controller/ProvidersController.php
+++ server/src/Controller/ProvidersController.php
@@ -9,14 +9,17 @@ use App\Entity\TransportProvider;
 use App\Repository\TransportProviderRepository;
 use Symfony\Component\HttpFoundation\JsonResponse;
 use Symfony\Component\HttpFoundation\Request;
+use Doctrine\ORM\EntityManagerInterface;
 
 #[Route('/api')]
 class ProvidersController extends AbstractController
 {   
     private TransportProviderRepository $providerRepository;
+    private EntityManagerInterface $entityManager;
 
-    public function __construct(TransportProviderRepository $providerRepository) {
+    public function __construct(TransportProviderRepository $providerRepository, EntityManagerInterface $entityManager,) {
         $this->providerRepository = $providerRepository;
+        $this->entityManager = $entityManager;
     }
 
     #[Route('/providers', name: 'api_providers', methods: ['GET'])]
@@ -63,5 +66,72 @@ class ProvidersController extends AbstractController
 
         return new JsonResponse($data);
 
+    }
+
+    #[Route('/provider', name: 'api_create_provider', methods: ['POST'])]
+    public function createProvider(Request $request): Response {
+
+        $data = json_decode($request->getContent(), true);
+
+        if (!isset($data['name'], $data['EAN'], $data['length'], $data['width'], 
+                    $data['height'], $data['price'], $data['MaxWeight'])) {
+                        
+            return new JsonResponse(['error', 'Données invalides'],Response::HTTP_BAD_REQUEST);
+        }
+
+        $provider = new TransportProvider();
+        $provider->setName($data['name']);
+        $provider->setEAN($data['EAN']);
+        $provider->setLength($data['length']);
+        $provider->setWidth($data['width']);
+        $provider->setHeight($data['height']);
+        $provider->setPrice($data['price']);
+        $provider->setMaxWeight($data['MaxWeight']);
+
+        $this->entityManager->persist($provider);
+        $this->entityManager->flush();
+
+        return new JsonResponse(['message' => 'Transporteur enregistré avec succès'], Response::HTTP_CREATED);
+
+    }
+
+    #[Route('/provider/{id}', name: 'api_update_provider', methods: ['PUT'])]
+    public function updateProvider(Request $request, int $id): Response {
+
+        $data = json_decode($request->getContent(), true);
+
+        $provider = $this->providerRepository->find($id);
+
+        if (!$provider) {
+            return new JsonResponse(['error' => 'Transporteur non trouvé'], JsonResponse::HTTP_NOT_FOUND);
+        }
+
+        $provider->setName($data['name']);
+        $provider->setEAN($data['EAN']);
+        $provider->setLength($data['length']);
+        $provider->setWidth($data['width']);
+        $provider->setHeight($data['height']);
+        $provider->setPrice($data['price']);
+        $provider->setMaxWeight($data['MaxWeight']);
+
+        $this->entityManager->persist($provider);
+        $this->entityManager->flush();
+
+        return new JsonResponse(['message' => 'Transporteur mis à jour avec succès'], JsonResponse::HTTP_OK);
+    }
+
+    #[Route('/provider/{id}', name: 'api_delete_provider', methods: ['DELETE'])]
+    public function deleteProvider(int $id): Response {
+
+        $provider = $this->providerRepository->find($id);
+
+        if (!$provider) {
+            return new JsonResponse(['error' => 'Transporteur non trouvé'], JsonResponse::HTTP_NOT_FOUND);
+        }
+
+        $this->entityManager->remove($provider);
+        $this->entityManager->flush();
+
+        return new JsonResponse(['message' => 'Transporteur supprimé avec succès'], JsonResponse::HTTP_OK);
     }
 }
